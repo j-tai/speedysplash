60a5caca093d917dba51f76a56aa82514c1d7e3a
Jasmine Tai <jtai@jtai.ca>
2021-07-06 19:30:29 -0700

    Update to Minecraft 1.17.1

--- src/main/java/ca/jtai/speedysplash/mixin/SplashScreenMixin.java
+++ src/main/java/ca/jtai/speedysplash/mixin/SplashScreenMixin.java
@@ -1,15 +1,16 @@
 package ca.jtai.speedysplash.mixin;
 
-import net.minecraft.client.gui.screen.SplashScreen;
+import net.minecraft.client.gui.screen.SplashOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SplashScreen.class)
+@Mixin(SplashOverlay.class)
 public class SplashScreenMixin {
-    @Shadow private long reloadCompleteTime;
+    @Shadow
+    private long reloadCompleteTime;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(CallbackInfo info) {
